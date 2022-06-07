9637e7f6782a9558829e5bdc82dbff5e8b8a9c51
Chen Heng <98751726+chenheng96@users.noreply.github.com>
2022-06-07 15:14:53 +0800

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchong <cchong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/06 10:29:44 by cchong            #+#    #+#             */
+/*   Updated: 2022/05/06 10:29:45 by cchong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *str, ...);
+
+#endif
